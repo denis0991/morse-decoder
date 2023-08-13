691a100a60e4c275b6f5cc0a0a307b22532b07dc
Denis Nasonov <brain0991@gmail.com>
2023-08-13 14:52:41 +0500

    feat: finished task

--- src/index.js
+++ src/index.js
@@ -37,8 +37,52 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const obj = {'10': '.', '11': '-'}
+
+function func(elem) {
+    const obj = {'10': '.', '11': '-'}
+    let result = '';
+    let decod = '';
+    let count = 0;
+    
+        for (let i = 0; i < elem.length; i+=2) {
+            if (elem[i] + elem[i+1] == 0) {
+            
+                count++
+                if(count % 5 == 0) {
+                    result += ' '
+                }
+            } else {
+                result += obj[elem[i] + elem[i+1]];
+                
+                count++
+                if(count % 5 == 0) {
+                    result += ' '  
+                }
+            }
+        }
+        return result
+   }
+
 function decode(expr) {
     // write your solution here
+    let arr = expr.split('**********')
+    let newArr = [];
+    let finishLine = '';
+        for (let elem of arr) {
+            newArr.push(elem.split(''))
+        }
+   for (let elem of newArr) {
+    let arraySymbol = func(elem).split(' ');
+    for (let i = 0; i < arraySymbol.length; i++) {
+        if (MORSE_TABLE[arraySymbol[i]]) {
+           finishLine += MORSE_TABLE[arraySymbol[i]]
+        } else {
+            finishLine += ' '
+        }
+    }
+   }
+   return finishLine.slice(0, -1)
 }
 
 module.exports = {
